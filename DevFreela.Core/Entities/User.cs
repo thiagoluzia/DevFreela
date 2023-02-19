03cb080ef90abf5a8f7ed897159799219aed0dec
Thiago Moura <thiago.mouraluzia@gmail.com>
2023-02-19 18:01:54 -0300

    Implementation layer core, also known as domain

---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullname, string email, DateTime birthDate)
+        {
+            Fullname = fullname;
+            Email = email;
+            BirthDate = birthDate;
+            Active = true;
+
+            Skills = new List<UserSkill>();
+            OwnerdProjects = new List<Projetc>();
+            FreelanceProjects = new List<Projetc>();
+
+        }
+
+        public string Fullname { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+         public bool Active { get; private set; }
+         
+        public List<UserSkill> Skills { get; private set; }
+        public List<Projetc> OwnerdProjects { get; private set; }
+        public List<Projetc> FreelanceProjects { get; private set; }
+       
+
+    }
+}
